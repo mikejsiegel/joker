1cee879989ea6f8e90fa3b666e805e735b033743
Mike Siegel <mike@jibe.com>
2011-10-13 18:44:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joker::Application.config.secret_token = 'c78305d95b2d419ce365e317cf04726a0fe0e039f648f3df4e16fef7a865a6310a7ad6ec07a37238dce898eb466f0a6324c7adc636f106a4db0f3bfb12ff80f4'
